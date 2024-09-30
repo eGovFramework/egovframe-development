dc701adbf40b98213e1ced24c6db04be96244958
likiwook <likiwook@daum.net>
2024-09-30 17:05:00 +0900

    Update DbioLog.java
    DbioLog.java 리팩토링(Refactoring)

--- egovframework.dev.imp.dbio/src/egovframework/dev/imp/dbio/common/DbioLog.java
+++ egovframework.dev.imp.dbio/src/egovframework/dev/imp/dbio/common/DbioLog.java
@@ -34,80 +34,72 @@ import egovframework.dev.imp.dbio.DBIOPlugin;
  *  -------    --------    ---------------------------
  *   2009.02.20  김형조          최초 생성
  *
- * 
  * </pre>
  */
 public class DbioLog {
 
-	/**
-	 * 로깅할 상태 생성
-	 * 
-	 * @param severity
-	 * @param code
-	 * @param message
-	 * @param exception
-	 * @return IStatus
-	 */
-	private static IStatus createStatus(int severity, int code, String message,
-			Throwable exception) {
-		IStatus status = new Status(severity, DBIOPlugin.PLUGIN_ID, code,
-				message, exception);
-		return status;
-	}
+    private static final int DEFAULT_CODE = IStatus.OK;
 
-	/**
-	 * 로깅 처리
-	 * 
-	 * @param status
-	 */
-	private static void log(IStatus status) {
-		DBIOPlugin.getDefault().getLog().log(status);
-		return;
-	}
+    /**
+     * 로깅할 상태 생성
+     * 
+     * @param severity 로그의 심각도
+     * @param code 상태 코드
+     * @param message 로그 메시지
+     * @param exception 예외 객체 (있는 경우)
+     * @return 생성된 IStatus 객체
+     */
+    private static IStatus createStatus(int severity, int code, String message, Throwable exception) {
+        return new Status(severity, DBIOPlugin.PLUGIN_ID, code, message, exception);
+    }
 
-	/**
-	 * 정보레벨 로깅처리
-	 * 
-	 * @param message
-	 */
-	public static void logInfo(String message) {
-		log(IStatus.INFO, IStatus.OK, message, null);
-		return;
-	}
+    /**
+     * 로깅 처리
+     * 
+     * @param status 로깅할 IStatus 객체
+     */
+    private static void logStatus(IStatus status) {
+        DBIOPlugin.getDefault().getLog().log(status);
+    }
 
-	/**
-	 * 에러레벨 로깅 처리
-	 * 
-	 * @param exception
-	 */
-	public static void logError(Throwable exception) {
-		logError("Unexpected Exception", exception); //$NON-NLS-1$
-		return;
-	}
+    /**
+     * 정보레벨 로깅처리
+     * 
+     * @param message 로그 메시지
+     */
+    public static void logInfo(String message) {
+        log(IStatus.INFO, DEFAULT_CODE, message, null);
+    }
 
-	/**
-	 * 에러레벨 로깅 처리
-	 * 
-	 * @param message
-	 * @param exception
-	 */
-	public static void logError(String message, Throwable exception) {
-		exception.printStackTrace();
-		log(IStatus.ERROR, IStatus.OK, message, exception);
-		return;
-	}
+    /**
+     * 에러레벨 로깅 처리
+     * 
+     * @param exception 예외 객체
+     */
+    public static void logError(Throwable exception) {
+        logError("Unexpected Exception", exception);
+    }
 
-	/**
-	 * 대표 로깅 처리
-	 * 
-	 * @param severity
-	 * @param code
-	 * @param message
-	 * @param exception
-	 */
-	public static void log(int severity, int code, String message,
-			Throwable exception) {
-		log(createStatus(severity, code, message, exception));
-		return;
-	}
+    /**
+     * 에러레벨 로깅 처리
+     * 
+     * @param message 에러 메시지
+     * @param exception 예외 객체
+     */
+    public static void logError(String message, Throwable exception) {
+        log(IStatus.ERROR, DEFAULT_CODE, message, exception);
+    }
+
+    /**
+     * 대표 로깅 처리
+     * 
+     * @param severity 로그의 심각도
+     * @param code 상태 코드
+     * @param message 로그 메시지
+     * @param exception 예외 객체 (있는 경우)
+     */
+    public static void log(int severity, int code, String message, Throwable exception) {
+        IStatus status = createStatus(severity, code, message, exception);
+        logStatus(status);
+    }
 }
